f1ada6ffedf1e95d0d14f6c011eb45651bca84f9
unknown <iamayushsingh03@gmail.com>
2023-04-24 23:55:01 +0530

    first commit

---
+++ dsaa.js
@@ -0,0 +1,11 @@
+
+//factory function to create node
+function createNode (actpower , reactpower , r , x){
+      let node = {
+        actpower : actpower,
+        reactpower : reactpower,
+        r : r,
+        x: x,
+      }
+      return node     
+}
